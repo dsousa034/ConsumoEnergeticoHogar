69e0e4c94159ac38647f17be4bcdfe43cfd3e12f
AssilAB02 <aasribri@myuax.com>
2024-10-17 15:00:57 +0200

    Actualizar dependencias

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,48 +1,4 @@
-plugins {
-    alias(libs.plugins.android.application)
-    alias(libs.plugins.kotlin.android)
-    alias(libs.plugins.kotlin.compose)
-    //id("com.android.application")
-    id("com.google.gms.google-services")
-}
-
-android {
-    namespace = "com.example.consumoenergticohogar"
-    compileSdk = 34
-
-    defaultConfig {
-        applicationId = "com.example.consumoenergticohogar"
-        minSdk = 21
-        targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-    kotlinOptions {
-        jvmTarget = "1.8"
-    }
-    buildFeatures {
-        compose = true
-    }
-}
-
 dependencies {
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
@@ -54,6 +10,11 @@ dependencies {
     implementation(libs.androidx.navigation.runtime.ktx)
     implementation(libs.androidx.navigation.compose)
     implementation(libs.androidx.storage)
+
+    // WorkManager para tareas en segundo plano
+    implementation("androidx.work:work-runtime-ktx:2.7.1")
+
+    // Dependencias de prueba
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
@@ -61,5 +22,7 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    // Firebase
     implementation(platform("com.google.firebase:firebase-bom:33.4.0"))
 }
